8d78414bf72d6f2b3346b7300ceb6f9eadb115c2
HessianThespian <adrianfriduson@gmail.com>
2022-11-23 15:58:41 +0100

    Experimenting with the navbar and not getting anywhere

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,31 +1,39 @@
 import { useEffect } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import { useUser } from "../../context/UserContext";
+import profile from "../../assets/profile-image.svg"
 
-const Navbar = () => {
 
+/* const HandleProfileClick = () => {
     const { user } = useUser()
-    /* const navigate = useNavigate()
+
+    const navigate = useNavigate()
 
     useEffect(() => {
         if (user !== null) {
             navigate('profile')
         }
-    }, [user, navigate]) */
+    }, [user, navigate])
+} */
+
+const Navbar = () => {
 
     return (
         <nav className='navBar'>
             <h1 class="animate__animated animate__bounce">The Signs</h1>
-            {user !== null &&
-                <span className="navButtons">
-                    <NavLink to="profile">Profile</NavLink>
-                    <br />
-                    <NavLink to="/translate">Translations</NavLink>
-                </span>
-            }
+
+            <span className="navButtons">
+                <NavLink to="profile">Profile</NavLink>
+                <br />
+                <NavLink to="/translate">Translations</NavLink>
+            </span>
+
         </nav>
 
     )
 }
+
+// <img src={profile} alt="" onClick={HandleProfileClick} />
+
 
 export default Navbar;
